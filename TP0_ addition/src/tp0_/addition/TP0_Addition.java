3d4136c39bb9fb3470d617b824b276d31e385c98
Neo Noshie <neo.noshie@epfedu.fr>
2025-10-22 15:54:36 +0200

    TP0_addition

---
+++ TP0_ addition/src/tp0_/addition/TP0_Addition.java
@@ -0,0 +1,33 @@
+/*helloWorld, Addition d’entiers, 20/10/2025*/
+package tp0_.addition;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author neono
+ */
+public class TP0_Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nb; // nombre d'entiers à additionner 
+        int result; // resultat 
+        int ind; //indice  
+    // initialisation 
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Entrer le nombre :");  
+        nb=sc.nextInt();  
+        result=0;  
+    // addition des nb premiers entiers  
+        ind=1; 
+        while (ind< nb) {  
+            result=result+ind;  
+            ind++;
+        }  // affichage du resultat  
+        System.out.println("la somme des "+ nb + " entiers est: "+result);
+    }
+    
+}
