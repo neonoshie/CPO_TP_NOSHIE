d37998429a5828a4051d95ef13cdfae5e19fbc76
Neo Noshie <neo.noshie@epfedu.fr>
2025-11-04 10:39:18 +0100

    TP2_biere

--- TP2_Bieres_NOSHIE/src/tp2_bieres_noshie/BouteilleBiere.java
+++ TP2_Bieres_NOSHIE/src/tp2_bieres_noshie/BouteilleBiere.java
@@ -1,38 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/**/
 package tp2_bieres_noshie;
 
-/**
- *
- * @author neono
- */
 public class BouteilleBiere {
     
     String nom; 
     double degreAlcool; 
     String brasserie;
+    boolean ouverte = false; 
     
     public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) { 
         nom = unNom; 
         degreAlcool = unDegre; 
-        brasserie = uneBrasserie; 
+        brasserie = uneBrasserie;
+        
     } 
-    public Decapsuler(boolean open) {
-        if (open == false){
-            open = true;
-        else 
-        
-            
+    public boolean Decapsuler(){
+        if ( ouverte == false){
+            ouverte = true;
         }
-        
+        else {
+            System.out.println("erreur : biere déjà ouverte ");
+        }
+        return false;
     }
-    
-     
 
 public void lireEtiquette() {
-    System.out.println("Bouteille de "+ nom + " ("+ degreAlcool + " degres) \nBrasserie : "+ brasserie);
+    System.out.println("Bouteille de "+ nom + " ("+ degreAlcool + " degres) "+ouverte+" \nBrasserie : "+ brasserie);
     
 }
+
+    @Override
+    public String toString() {
+       String chaine_a_retourner; 
+       chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";   
+       if (ouverte == true ) chaine_a_retourner += "oui" ; 
+       else chaine_a_retourner += "non" ; 
+       return chaine_a_retourner ;
+    }
+
 }
--- TP2_Bieres_NOSHIE/src/tp2_bieres_noshie/TP2_Bieres_NOSHIE.java
+++ TP2_Bieres_NOSHIE/src/tp2_bieres_noshie/TP2_Bieres_NOSHIE.java
@@ -12,8 +12,10 @@ public class TP2_Bieres_NOSHIE {
      */
     public static void main(String[] args) {
         
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0, "Dubuisson") ;
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0, "Dubuisson");
+        uneBiere.Decapsuler();
         uneBiere.lireEtiquette();
+        
         
         BouteilleBiere deuxBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe");
         deuxBiere.lireEtiquette();
@@ -22,6 +24,7 @@ public class TP2_Bieres_NOSHIE {
         troisBiere.lireEtiquette();
         
         BouteilleBiere quatreBiere = new BouteilleBiere("1664", 7.6,"Le louvre");
+        quatreBiere.Decapsuler();
         quatreBiere.lireEtiquette();
         
         BouteilleBiere cinqBiere = new BouteilleBiere("Corona", 6.9,"Voiture");
