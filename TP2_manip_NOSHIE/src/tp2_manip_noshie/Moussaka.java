ba259689bb73f67fbe53a82b96753fa0b05ab0b1
Neo Noshie <neo.noshie@epfedu.fr>
2025-11-05 20:13:24 +0100

    TP2_kcal

---
+++ TP2_manip_NOSHIE/src/tp2_manip_noshie/Moussaka.java
@@ -0,0 +1,11 @@
+/**/
+package tp2_manip_noshie;
+
+
+public class Moussaka {
+    int nbCalories;
+    
+    public Moussaka(int kcal){
+        nbCalories = kcal;
+    }
+}
---
+++ TP2_manip_NOSHIE/src/tp2_manip_noshie/Tartiflette.java
@@ -0,0 +1,10 @@
+/**/
+package tp2_manip_noshie;
+
+public class Tartiflette {
+    int nbCalories;
+    
+    public Tartiflette(int kcal) {
+    nbCalories = kcal;
+}
+}
