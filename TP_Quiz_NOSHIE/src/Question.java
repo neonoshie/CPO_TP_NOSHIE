c42f688f76de82fec12a5cb13626a79e64bceddd
Neo Noshie <neo.noshie@epfedu.fr>
2025-11-26 18:33:18 +0100

    TP interface

---
+++ TP_Quiz_NOSHIE/src/Question.java
@@ -0,0 +1,45 @@
+/**/
+
+public class Question {
+  private String intitule;
+  private String proposition1;
+  private String proposition2;
+  private String proposition3;
+  private String proposition4;
+  private int indexBonneReponse;  
+
+    public Question(String intitule, String p1, String p2, String p3, String p4, int indexBonneReponse) {
+        this.intitule = intitule;
+        this.proposition1 = p1;
+        this.proposition2 = p2;
+        this.proposition3 = p3;
+        this.proposition4 = p4;
+        this.indexBonneReponse = indexBonneReponse;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public String getProposition1() {
+        return proposition1;
+    }
+
+    public String getProposition2() {
+        return proposition2;
+    }
+
+    public String getProposition3() {
+        return proposition3;
+    }
+
+    public String getProposition4() {
+        return proposition4;
+    }
+
+    public int getIndexBonneReponse() {
+        return indexBonneReponse;
+    }
+
+  
+}
